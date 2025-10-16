6a42e82df3f6f226f6caddc1fe63c4838cbbb096
msalah-kw <msalah00@mail.com>
2025-10-16 12:14:07 +0300

    remove bloats

---
+++ inc/integrations/gutenberg/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-notices', 'wp-plugins', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => 'db9958fe9c196b4a550d');
